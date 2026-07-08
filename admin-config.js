838be4f539e722b20ac85166437097f9a015bcca
Owoyemi Samuel Tobi <tobiopeyemi057@gmail.com>
2026-07-08 15:55:34 +0100

    Configure admin console for production Supabase

--- admin-config.js
+++ admin-config.js
@@ -5,4 +5,4 @@ window.__JOBBRIDGE_SUPABASE_URL__ =
 // Set this to the public anon key for the same Supabase project.
 // Do NOT put the service-role key here.
 window.__JOBBRIDGE_SUPABASE_ANON_KEY__ =
-  window.__JOBBRIDGE_SUPABASE_ANON_KEY__ || "";
+  window.__JOBBRIDGE_SUPABASE_ANON_KEY__ || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwcmFtb211Y2tranpzc3JmZ2hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzODU2MjcsImV4cCI6MjA5Nzk2MTYyN30.8cWGk5Kbn4YmJKw5XU5FnfWZzCM1mDpOJZRe1PUUlgs";
