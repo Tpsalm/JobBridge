f035668cc38c145ee45a3d5e0814f641dc89a14b
Owoyemi Samuel Tobi <tobiopeyemi057@gmail.com>
2026-07-14 16:24:19 +0100

    admin: ensure admin alias and login fixes are committed

--- admin-config.js
+++ admin-config.js
@@ -6,3 +6,6 @@ window.__JOBBRIDGE_SUPABASE_URL__ =
 // Do NOT put the service-role key here.
 window.__JOBBRIDGE_SUPABASE_ANON_KEY__ =
   window.__JOBBRIDGE_SUPABASE_ANON_KEY__ || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwcmFtb211Y2tranpzc3JmZ2hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzODU2MjcsImV4cCI6MjA5Nzk2MTYyN30.8cWGk5Kbn4YmJKw5XU5FnfWZzCM1mDpOJZRe1PUUlgs";
+
+window.__JOBBRIDGE_SUPABASE_ADMIN_EMAIL__ =
+  window.__JOBBRIDGE_SUPABASE_ADMIN_EMAIL__ || "tobiopeyemi057@gmail.com";
